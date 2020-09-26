c2d49eae4edfc0872b08f515a3a9ea983d197f76
sirlocust <jua56yu@hotmail.com>
2020-09-26 02:54:10 -0500

    creando el paquete modelo y la clase Vacante

---
+++ src/main/java/io/devsirlocust/empleos/model/Vacante.java
@@ -0,0 +1,59 @@
+package io.devsirlocust.empleos.model;
+
+import java.util.Date;
+
+public class Vacante {
+    
+    private Integer id;
+    private String name;
+    private String description;
+    private Date date;
+    private Double wage;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getWage() {
+        return wage;
+    }
+
+    public void setWage(Double wage) {
+        this.wage = wage;
+    }
+
+    @Override
+    public String toString() {
+        return "Vacante [date=" + date + ", description=" + description + ", id=" + id + ", name=" + name + ", wage="
+                + wage + "]";
+    }
+    
+}
