8511484bb6848d47b431b760c50bc36b8b13d3bc
sirlocust <jua56yu@hotmail.com>
2020-09-29 02:58:21 -0500

    se crea una lista de tipo vacante y se despliega en la vista con th:each

--- src/main/java/io/devsirlocust/empleos/controller/HomeController.java
+++ src/main/java/io/devsirlocust/empleos/controller/HomeController.java
@@ -13,9 +13,22 @@ import io.devsirlocust.empleos.model.Vacante;
 @Controller
 public class HomeController {
     
+    @GetMapping("/tabla")
+    public String showTable(Model model){
+        final List<Vacante> lista = getVacantes();
+
+        model.addAttribute("vacantes", lista);
+
+        return "tabla";
+
+
+    }
+
+
     @GetMapping("/detalle")
     public String showDetail(final Model model){
         final Vacante vacante = new Vacante();
+        
         vacante.setName("ingeniero de comunicaciones");
         vacante.setDescription("se solicita ingeniero parada dar soporte");
         vacante.setDate(new Date());
@@ -52,5 +65,48 @@ public class HomeController {
         model.addAttribute("wage", wage);
         model.addAttribute("valid", valid);
         return "home";
+    }
+
+
+    private List<Vacante> getVacantes(){
+        List<Vacante> lista = new LinkedList<>();
+
+        try {
+
+            final Vacante vacante = new Vacante();
+            vacante.setId(1);
+
+            vacante.setName("ingeniero de comunicaciones");
+            vacante.setDescription("se solicita ingeniero parada dar soporte");
+            vacante.setDate(new Date());
+            vacante.setWage(9700.0);
+
+            final Vacante vacante1 = new Vacante();
+            vacante1.setId(1);
+
+            vacante1.setName("ingeniero de comunicaciones");
+            vacante1.setDescription("se solicita ingeniero parada dar soporte");
+            vacante1.setDate(new Date());
+            vacante1.setWage(9700.0);
+
+            final Vacante vacante2 = new Vacante();
+            vacante2.setId(1);
+
+            vacante2.setName("ingeniero de comunicaciones");
+            vacante2.setDescription("se solicita ingeniero parada dar soporte");
+            vacante2.setDate(new Date());
+            vacante2.setWage(9700.0);
+
+
+            lista.add(vacante);
+            lista.add(vacante1);
+            lista.add(vacante2);
+
+
+        } catch (Exception e) {
+            
+            System.out.println(e);
+        }
+        return lista;
     }
 }
