6de67225a31d7a7f3e1d57ac06f6183886c6c3c3
sirlocust <jua56yu@hotmail.com>
2020-10-13 12:34:29 -0500

    integrar el formulario

--- src/main/java/io/devsirlocust/empleos/controller/CategoriasController.java
+++ src/main/java/io/devsirlocust/empleos/controller/CategoriasController.java
@@ -5,25 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/categoria")
 public class CategoriasController {
-    
 
     @GetMapping("/index")
-    public String showIndex(Model model){
+    public String showIndex(Model model) {
         return "categorias/listCategorias";
     }
-    
+
     @GetMapping("/create")
     // @RequestMapping(value = "/create", method = RequestMethod.GET)
-    public String create(){
+    public String create() {
         return "categorias/formCategorias";
     }
 
     @PostMapping("/save")
-    public String save(){
+    // @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(@RequestParam("name") String name, @RequestParam("description") String description) {
+        System.out.println(name);
         return "categorias/listCategorias";
     }
 }
